4cd6631b0d9831c0a4130abc1ea106a68593911d
sippnex <julian.sobotka@gmail.com>
2021-08-27 12:32:54 +0200

    wip

--- static/photo-page.js
+++ static/photo-page.js
@@ -1,14 +1,14 @@
-const countdown = document.querySelector("#photo-countdown");
-countdown.innerHTML = '10';
-setTimeout(() => countdown.innerHTML = '9', 1000);
-setTimeout(() => countdown.innerHTML = '8', 2000);
-setTimeout(() => countdown.innerHTML = '7', 3000);
-setTimeout(() => countdown.innerHTML = '6', 4000);
-setTimeout(() => countdown.innerHTML = '5', 5000);
-setTimeout(() => countdown.innerHTML = '4', 6000);
-setTimeout(() => countdown.innerHTML = '3', 7000);
-setTimeout(() => countdown.innerHTML = '2', 8000);
-setTimeout(() => countdown.innerHTML = '1', 9000);
+const photoCountdown = document.querySelector("#photo-countdown");
+photoCountdown.innerHTML = '10';
+setTimeout(() => photoCountdown.innerHTML = '9', 1000);
+setTimeout(() => photoCountdown.innerHTML = '8', 2000);
+setTimeout(() => photoCountdown.innerHTML = '7', 3000);
+setTimeout(() => photoCountdown.innerHTML = '6', 4000);
+setTimeout(() => photoCountdown.innerHTML = '5', 5000);
+setTimeout(() => photoCountdown.innerHTML = '4', 6000);
+setTimeout(() => photoCountdown.innerHTML = '3', 7000);
+setTimeout(() => photoCountdown.innerHTML = '2', 8000);
+setTimeout(() => photoCountdown.innerHTML = '1', 9000);
 setTimeout(() => {
   xhttp.open("DELETE", "/api/photo", true);
   xhttp.addEventListener("load", () => window.location.href="/");
