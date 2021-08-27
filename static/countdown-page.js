673fa397337ba63b93edba82aace51854e4f440e
sippnex <julian.sobotka@gmail.com>
2021-08-27 12:56:02 +0200

    wip

--- static/countdown-page.js
+++ static/countdown-page.js
@@ -3,32 +3,32 @@ if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(stream => {
       preview.srcObject = stream;
-      const countdown = document.querySelector("#countdown-text");
-      countdown.innerHTML = '5';
-      setTimeout(() => countdown.innerHTML = '4', 1000);
-      setTimeout(() => {
-        countdown.innerHTML = '3';
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "/api/led-on", true);
-        xhttp.send();
-      }, 2000);
-      setTimeout(() => countdown.innerHTML = '2', 3000);
-      setTimeout(() => countdown.innerHTML = '1', 4000);
-      setTimeout(() => {
-        countdown.innerHTML = '';
-        stream.getTracks().forEach(track => {
-          track.stop();
-        });
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "/api/take-photo", true);
-        xhttp.addEventListener("load", () => {
+      const countdownElement = document.querySelector("#countdown-text");
+      let countdown = 5;
+      countdownElement.innerHTML = countdown;
+      setInterval(() => {
+        countdown--;
+        if (countdown > 0) {
+          countdownElement.innerHTML = countdown;
+          if (countdown === 3) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.open("GET", "/api/led-on", true);
+            xhttp.send();
+          }
+        } else {
+          countdownElement.innerHTML = '';
+          document.querySelector("#countdown-flash").style.display = 'initial';
           var xhttp = new XMLHttpRequest();
-          xhttp.open("GET", "/api/led-off", true);
-          xhttp.addEventListener("load", () => window.location.href="photo-page");
+          xhttp.open("GET", "/api/take-photo", true);
+          xhttp.addEventListener("load", () => {
+            var xhttp = new XMLHttpRequest();
+            xhttp.open("GET", "/api/led-off", true);
+            xhttp.addEventListener("load", () => window.location.href = "photo-page");
+            xhttp.send();
+          });
           xhttp.send();
-        });
-        xhttp.send();
-      }, 5000);
+        }
+      }, 1000);
     })
     .catch(error => {
       console.error("Something went wrong!", error);
--- static/photo-page.js
+++ static/photo-page.js
@@ -1,23 +1,20 @@
-const photoCountdown = document.querySelector("#photo-countdown");
-photoCountdown.innerHTML = '10';
-setTimeout(() => photoCountdown.innerHTML = '9', 1000);
-setTimeout(() => photoCountdown.innerHTML = '8', 2000);
-setTimeout(() => photoCountdown.innerHTML = '7', 3000);
-setTimeout(() => photoCountdown.innerHTML = '6', 4000);
-setTimeout(() => photoCountdown.innerHTML = '5', 5000);
-setTimeout(() => photoCountdown.innerHTML = '4', 6000);
-setTimeout(() => photoCountdown.innerHTML = '3', 7000);
-setTimeout(() => photoCountdown.innerHTML = '2', 8000);
-setTimeout(() => photoCountdown.innerHTML = '1', 9000);
-setTimeout(() => {
-  xhttp.open("DELETE", "/api/photo", true);
-  xhttp.addEventListener("load", () => window.location.href="/");
-  xhttp.send();
-}, 10000);
-
 const xhttp = new XMLHttpRequest();
 xhttp.open("GET", "/api/photo", true);
 xhttp.addEventListener("load", () => {
   document.querySelector('#photo-preview').src = "data:image/png;base64," + xhttp.response;
 });
 xhttp.send();
+
+const countdownElement = document.querySelector("#photo-countdown");
+let countdown = 10;
+countdownElement.innerHTML = countdown;
+setInterval(() => {
+  countdown--;
+  if (countdown > 0) {
+    countdownElement.innerHTML = countdown;
+  } else {
+    xhttp.open("DELETE", "/api/photo", true);
+    xhttp.addEventListener("load", () => window.location.href="/");
+    xhttp.send();
+  }
+}, 1000);
