3dc535751ed011a74e8e4dbb963c2d510641b335
sippnex <julian.sobotka@gmail.com>
2021-08-27 13:20:40 +0200

    wip

--- static/countdown-page.js
+++ static/countdown-page.js
@@ -1,6 +1,6 @@
-let xhttp = new XMLHttpRequest();
-xhttp.open("GET", "/api/led-on", true);
-xhttp.send();
+const xhttp1 = new XMLHttpRequest();
+xhttp1.open("GET", "/api/led-on", true);
+xhttp1.send();
 const preview = document.querySelector("#countdown-preview");
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
@@ -9,22 +9,29 @@ if (navigator.mediaDevices.getUserMedia) {
       const countdownElement = document.querySelector("#countdown-text");
       let countdown = 5;
       countdownElement.innerHTML = countdown;
-      setInterval(() => {
+      const interval = setInterval(() => {
         countdown--;
         if (countdown > 0) {
           countdownElement.innerHTML = countdown;
         } else {
+          clearInterval(interval);
           countdownElement.innerHTML = '';
           document.querySelector("#countdown-flash").style.display = 'initial';
-          xhttp = new XMLHttpRequest();
-          xhttp.open("GET", "/api/take-photo", true);
-          xhttp.addEventListener("load", () => {
-            xhttp = new XMLHttpRequest();
-            xhttp.open("GET", "/api/led-off", true);
-            xhttp.addEventListener("load", () => window.location.href = "photo-page");
-            xhttp.send();
-          });
-          xhttp.send();
+          const xhttp2 = new XMLHttpRequest();
+          xhttp2.open("GET", "/api/take-photo", true);
+          xhttp2.onreadystatechange = () => {
+            if (xhttp2.readyState == 4) {
+              const xhttp3 = new XMLHttpRequest();
+              xhttp3.open("GET", "/api/led-off", true);
+              xhttp3.onreadystatechange = () => {
+                if (xhttp3.readyState == 4) {
+                  window.location.href = "photo-page"
+                }
+              };
+              xhttp3.send();
+            }
+          };
+          xhttp2.send();
         }
       }, 1000);
     })
--- static/photo-page.js
+++ static/photo-page.js
@@ -1,20 +1,27 @@
-const xhttp = new XMLHttpRequest();
-xhttp.open("GET", "/api/photo", true);
-xhttp.addEventListener("load", () => {
-  document.querySelector('#photo-preview').src = "data:image/png;base64," + xhttp.response;
-});
-xhttp.send();
-
-const countdownElement = document.querySelector("#photo-countdown");
-let countdown = 10;
-countdownElement.innerHTML = countdown;
-setInterval(() => {
-  countdown--;
-  if (countdown > 0) {
+const xhttp1 = new XMLHttpRequest();
+xhttp1.open("GET", "/api/photo", true);
+xhttp1.onreadystatechange = () => {
+  if (xhttp1.readyState == 4) {
+    document.querySelector('#photo-preview').src = "data:image/png;base64," + xhttp1.response;
+    const countdownElement = document.querySelector("#photo-countdown");
+    let countdown = 10;
     countdownElement.innerHTML = countdown;
-  } else {
-    xhttp.open("DELETE", "/api/photo", true);
-    xhttp.addEventListener("load", () => window.location.href="/");
-    xhttp.send();
+    const interval = setInterval(() => {
+      countdown--;
+      if (countdown > 0) {
+        countdownElement.innerHTML = countdown;
+      } else {
+        clearInterval(interval);
+        const xhttp2 = new XMLHttpRequest();
+        xhttp2.open("DELETE", "/api/photo", true);
+        xhttp2.onreadystatechange = () => {
+          if (xhttp2.readyState == 4) {
+            window.location.href = "/";
+          }
+        };
+        xhttp2.send();
+      }
+    }, 1000);
   }
-}, 1000);
+};
+xhttp1.send();
